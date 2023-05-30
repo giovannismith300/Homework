a24a3bb6775ec74b92b937bffbb0bb0dba2ca334
Giovanni Smith <giovanni.smith@duke.edu>
2023-05-30 13:08:34 +0200

    Day one homework added

---
+++ Day One Homework /node.js
@@ -0,0 +1,7 @@
+function changeBackground(){
+    let colors = ["red", "cyan", "blue", "green", "yellow", "purple", "pink", "orange", "white", "PaleGreen"]
+    let num = Math.floor( Math.random() * 10);
+    let code = "linear-gradient(45deg, " + colors[num] + ", black)";
+
+    document.getElementById("imageTab").style.background = code;
+ }
